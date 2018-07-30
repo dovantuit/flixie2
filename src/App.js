a1031a0aa7f4f74b13b848ef20f40cc4eac6c1cf
Tom <32444267+dovantuit@users.noreply.github.com>
2018-07-30 19:09:35 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Button } from 'reactstrap';
 import { Container } from 'bloomer';
 import "bulma/css/bulma.css";
 import MoviesList from "./MoviesList";
+import { InputGroup, InputGroupAddon, Input } from 'reactstrap';
 //import TEST_DATA from './test_json';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
     super(props);
     this.state = {
       movies: [],
+      filteredMovie: [],
       loading: true
     }
   }
@@ -19,35 +21,56 @@ class App extends Component {
   sleep(ms) {
     return new Promise(resolve =>setTimeout(resolve, ms))
   }
-// ham tu dong loading du lieu
+// ham tu dong loading du lieu 
    async componentDidMount() {
     const results = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed');
     const data =  await results.json();
     this.movies = data.results;
     await this.sleep(1000);
-    this.setState({
+    await this.setState({
       movies: this.movies,
+      filteredMovie: this.movies,
       loading: false
     });
+    console.log(this.state.filteredMovie);
+
   }
+  // ham loc du lieu
+  filterMovies(text){
+    let moviesCopy = this.state.movies;
+    let results = moviesCopy.filter( movie => movie.title.toLowerCase().includes(text.toLowerCase()));
+    this.setState({filteredMovie: results});
+  };
+
+
 
   render() {
     //let movies = TEST_DATA.results;
     let content;
     // kiem tra xem trang loading xong chua
     if( this.state.loading){
-      content=<h1>I'm loading</h1>
+      content=<h1>I'm loading, just wait a bit</h1>
     } else {
-      content = <MoviesList movies={this.state.movies}/>
+      content = <MoviesList movies={this.state.filteredMovie}/>
     }
 
     return (
       <Container>
+        
+      
+      
+    
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to tommy</h1>
+            <h1 className="App-title">Flixie</h1>
           </header>
+          <InputGroup>
+        <Input type='text' onChange={(text) => this.filterMovies(text.target.value)} />
+        <InputGroupAddon addonType="append">
+          
+        </InputGroupAddon>
+      </InputGroup>
           <Container>
             // noi dung hien thi lay tu render 
             {content}
